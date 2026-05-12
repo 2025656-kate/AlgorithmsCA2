bdbb24448e554b16660b859c1590994f0666db89
2025656-kate <2025656@student.cct.ie>
2026-05-12 14:47:15 +0100

    Create CA2_App.java
    Initial Netbeans project setup created

---
+++ CA2_App/src/main/java/com/mycompany/ca2_app/CA2_App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ca2_app;
+
+/**
+ *
+ * @author katha
+ */
+public class CA2_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
