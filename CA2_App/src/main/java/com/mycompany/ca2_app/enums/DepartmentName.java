3293beca4354bfc7a6d0016125efb7f1463ec69c
2025656-kate <2025656@student.cct.ie>
2026-05-13 11:48:11 +0100

    Create Enums that has - MEnuOption, ManagerType and DepartmentName

---
+++ CA2_App/src/main/java/com/mycompany/ca2_app/enums/DepartmentName.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ca2_app.enums;
+
+/**
+ *
+ * @author katha
+ */
+public class DepartmentName {
+    
+}
