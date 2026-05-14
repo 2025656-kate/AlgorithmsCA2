c2bd57e85056e1e0b98246254b8875944bbc6860
2025656-kate <2025656@student.cct.ie>
2026-05-14 10:39:28 +0100

    Created child class extended from employee called Admin Staff

---
+++ CA2_App/src/main/java/com/mycompany/ca2_app/AdminStaff.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ca2_app;
+import com.mycompany.ca2_app.enums.ManagerType;
+import com.mycompany.ca2_app.enums.DepartmentName;
+/**
+ *
+ * @author katha
+ */
+//Child of Employee. Extends it and add an office number
+public class AdminStaff extends Employee {
+    private String officeNumber;
+    
+    public AdminStaff(String firstName, String lastName,
+            ManagerType managerType, DepartmentName department,
+            String officeNumber){
+        super(firstName, lastName, managerType, department);
+        this.officeNumber = officeNumber;
+    }
+    
+    public String getOfficeNumber(){ return officeNumber;}
+    
+    @Override
+    public String toString(){
+        return super.toString() + "| Admin Staff (Office " + officeNumber + "}";
+
+    }
+}
