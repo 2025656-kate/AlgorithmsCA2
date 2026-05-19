6587d68990c4fdbb32b6fae10f62784a0f21e5ce
2025656-kate <2025656@student.cct.ie>
2026-05-19 13:47:20 +0100

    Main App with the load of file code

--- CA2_App/src/main/java/com/mycompany/ca2_app/CA2_App.java
+++ CA2_App/src/main/java/com/mycompany/ca2_app/CA2_App.java
@@ -4,13 +4,96 @@
 
 package com.mycompany.ca2_app;
 
-/**
- *
- * @author katha
- */
+//import enum
+import com.mycompany.ca2_app.enums.MenuOption;
+import com.mycompany.ca2_app.enums.ManagerType;
+import com.mycompany.ca2_app.enums.DepartmentName;
+import java.io.File;
+import java.io.FileNotFoundException;
+//import the utilities classes
+
+//for dinamic size list
+import java.util.ArrayList;
+
+//interface for list
+import java.util.List;
+
+//for reading user input from the keyboard
+import java.util.Scanner;
+
+
+
+//Main class for the School Application. It has the program menu 
 public class CA2_App {
 
+    //Static fields//
+    
+   //List with all employees loaded into the system. belongs to the class not to an instance
+    private static List<Employee> employees = new ArrayList<>();
+    
+    //Scanner to read inputs from keyboard
+    private static Scanner scanner = new Scanner(System.in);
+    
+    //Check if list is already sorted
+    private static boolean isSorted = false;
+    
+    
+    //////MAIN PROGRAM /////
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("==========SCHOOL ORGANISATION APPLICATION==========");
+    
+        //Ask the user for applicants file and load it 
+        loadApplicantsFile();
+        
+        //Show the main menu until the user choose exit 
+        runMenuLoop();
+        
+        //Message when the user decide to close the program
+        System.out.println("Thank you. Exiting the application...");
     }
-}
+    
+    /////LOAD THE APPLICANTS FORM/////
+    //The user will be asked for the file name, it will open from the resources folder
+    // add each row as employee to the list
+    private static void loadApplicantsFile(){
+                    
+            //Ask the user the name of the file they want 
+            System.out.println("Please enter the filename to read:");
+            String filename = scanner.nextLine().trim();
+            
+            try {
+                //Open the file with scanner
+                Scanner fileScanner = new Scanner(new File("src/main/resources/" + filename));
+                //Skip the first line because is only the line with the labels
+                fileScanner.nextLine();
+                
+                //read the remaining lines
+                while (fileScanner.hasNextLine()){
+                    String line = fileScanner.nextLine();
+                    //the lines will be split by commas
+                    String[] parts = line.split(",");
+                    
+                    //Extract the desired columns
+                    String firstName = parts[0].trim();
+                    String lastName = parts[1].trim();
+                    DepartmentName dept = DepartmentName.fromText(parts[5].trim());
+                    ManagerType type = ManagerType.fromText(parts[6].trim());
+                    
+                    //Create the new employee and add it to the list 
+                    Employee employee = new Employee(firstName, lastName, type, dept);
+                    employees.add(employee);
+                }
+                
+                fileScanner.close();
+                System.out.println(employees.size() + " employees loaded successfully.");
+                // if a new data (employee) is added the list still not sorted yet, unless the user do it
+                isSorted = false; 
+                
+            } catch (FileNotFoundException e){
+                System.out.println("File not found. Please try again");
+            }
+    
+                    
+               
+    
